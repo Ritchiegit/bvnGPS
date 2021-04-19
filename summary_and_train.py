690ec2a790934cdc5542fc4a06d11b3780226621
Qizhi Li <34611669+Ritchiegit@users.noreply.github.com>
2021-04-19 09:56:37 +0800

    Update summary_and_train.py

--- summary_and_train.py
+++ summary_and_train.py
@@ -101,10 +101,8 @@ def summary_and_train(train_data_all, test_data_all, label_train, label_test, re
     pickle.dump(clf_RF, open(model_save_file_path, "wb"))
 
     # SVM with one vs. one
-    # ‘linear’, ‘poly’, ‘rbf’, ‘sigmoid’,  # ‘precomputed’ precomputed 需要预先计算kernel的值
     kernel_list = ["linear"]  #  "poly", "rbf", "sigmoid"]
     decision_function_shape_list = ["ovo", "ovr"]
-    C_list = [1]
     C_list = [0.1, 0.2, 0.3, 0.4, 0.5, 0.6, 0.7, 0.8, 0.9, 1.0, 1.1, 1.2, 1.3, 1.4, 1.5, 1.6, 1.7, 1.8, 1.9, 2.0]
     for kernel in kernel_list:
         for decision_function_shape in decision_function_shape_list:
@@ -119,68 +117,7 @@ def summary_and_train(train_data_all, test_data_all, label_train, label_test, re
 
                 model_save_file_path = model_save_folder_path + model_name + ".model_pickle"
                 pickle.dump(clf_svm, open(model_save_file_path, "wb"))
-    # SVM with one vs. rest
 
-
-    # Multi layer FCN
-    """
-    learning_rate = 0.001
-    optimizer_str = "Adam"
-    hidden_feature_list = [8, 4]
-    y_pred, end_epoch = NN_2layer_train_test(X_train, X_test, y_train, y_test, num_classes, 2000, sklearn_random=sklearn_random,
-                                             criterion_type="MSE", learning_rate=learning_rate, optimizer_str=optimizer_str,
-                                             model_str="multi_layers_FCN", hidden_feature_list=hidden_feature_list, model_save_folder_path=model_save_folder_path, model_name_for_save=model_name)
-    model_name = f"MFCN_MSE_opt{optimizer_str}_h{hidden_feature_list}_lr{learning_rate}"
-    print(model_name)
-    multi_eval(y_test, y_pred, result_full_filepath, model_name, end_epoch)
-    """
-
-    """
-    # MMoE
-    optimizer_str_list = ["Adam", "Adagrad"]
-    learning_rate = 0.001
-    num_experts_list = [16, 24, 32]  # [2, 4, 8, 12]
-    # num_experts_list = [2, 4, 8, 12]
-    expert_unit_list = [4, 8, 12, 16, 32]  # expert units
-    hidden_units_list = [8, 16, 32]  # tower units
-    hidden_feature = ""
-    for optimizer_str in optimizer_str_list:
-        for num_experts in num_experts_list:
-            for expert_unit in expert_unit_list:
-                for hidden_units in hidden_units_list:
-                    config = Config(num_experts=num_experts, expert_unit=expert_unit, hidden_units=hidden_units)
-                    model_name = f"MMoE_MSE_opt{optimizer_str}_h{hidden_feature}_lr{learning_rate}_nexp{num_experts}_uexp{expert_unit}_utower{hidden_units}"
-                    print(model_name)
-                    y_pred, end_epoch = NN_2layer_train_test(X_train, X_test, y_train, y_test, num_classes, 2000,
-                                                  sklearn_random=sklearn_random, criterion_type="MSE",
-                                                  hidden_feature=hidden_feature, learning_rate=learning_rate,
-                                                  optimizer_str=optimizer_str, model_str="MMoE", config=config, model_save_folder_path=model_save_folder_path, model_name_for_save=model_name)
-                    multi_eval(y_test, y_pred, result_full_filepath, model_name, end_epoch)
-    """
-
-    # hidden_feature = 4
-    # optimizer_str = "Adam"
-    # learning_rate = 0.001
-    # model_name = f"NeuralNetworkMSE_opt{optimizer_str}_h{hidden_feature}_lr{learning_rate}"
-    # print(model_name)
-    # y_pred = NN_2layer_train_test(X_train, X_test, y_train, y_test, num_classes, 2000,
-    #                               sklearn_random=sklearn_random, criterion_type="MSE",
-    #                               hidden_feature=hidden_feature, learning_rate=learning_rate,
-    #                               optimizer_str=optimizer_str, model_str="FCN", model_save_folder_path=model_save_folder_path, model_name_for_save=model_name)
-    # multi_eval(y_test, y_pred, result_full_filepath, model_name)
-
-    """
-    hidden_feature = 4
-    optimizer_str = "Adam"
-    learning_rate = 0.001
-    model_name = f"NeuralNetworkMSE_opt{optimizer_str}_h{hidden_feature}_lr{learning_rate}"
-    print(model_name)
-    y_pred, end_epoch = NN_2layer_train_test(X_train, X_test, y_train, y_test, num_classes, 2000,
-                                  sklearn_random=sklearn_random, criterion_type="MSE",
-                                  hidden_feature=hidden_feature, learning_rate=learning_rate,
-                                  optimizer_str=optimizer_str, model_str="MoE", model_save_folder_path=model_save_folder_path, model_name_for_save=model_name)
-    multi_eval(y_test, y_pred, result_full_filepath, model_name, end_epoch)
-    """
     hidden_feature_to_search = [2, 4, 8, 16, 32, 64, 128, 256, 384, 512, 640, 768, 1024]
     # hidden_feature_to_search = [4, 16, 64, 256, 640, 1024]
     # hidden_feature_to_search = [2,8, 32, 128, 256, 500, 1000]
@@ -197,43 +134,6 @@ def summary_and_train(train_data_all, test_data_all, label_train, label_test, re
                 y_pred, end_epoch = NN_2layer_train_test(X_train, X_test, y_train, y_test, num_classes, 2000, sklearn_random=sklearn_random, criterion_type="MSE", hidden_feature=hidden_feature, learning_rate=learning_rate, optimizer_str=optimizer_str,
                                                          model_save_folder_path=model_save_folder_path, model_name_for_save=model_name)
                 multi_eval(y_test, y_pred, result_full_filepath, model_name, end_epoch)
-    # optimizer_str = "Adagrad"
-    # for max_epoch in [1, 2, 3, 4, 5, 6, 7, 10, 15, 20, 25, 35, 40, 50, 75, 100, 125, 150, 175, 200, 225, 250, 300, 400, 500, 600, 700, 800, 900, 1000, 1200, 1500, 2000, 3000]:
-    #     # Neural Network
-    #     hidden_feature = 96
-    #     learning_rate = 0.001
-    #     model_name = f"Neural Network MSE epoch{max_epoch}_opt{optimizer_str}"
-    #     print(model_name)
-    #     y_pred, end_epoch = NN_2layer_train_test(X_train, X_test, y_train, y_test, num_classes, max_epoch, sklearn_random=sklearn_random, criterion_type="MSE", hidden_feature=hidden_feature, learning_rate=learning_rate,
-    #                                   earlystop_turn_on=False, val_ratio=0, optimizer_str=optimizer_str, model_save_folder_path=model_save_folder_path, model_name_for_save=model_name)
-    #     multi_eval(y_test, y_pred, result_full_filepath, model_name, end_epoch)
-
-    # # lgb multi-class
-    # model_name = "lightgbm"
-    # print(model_name)
-    # gbm = lgb.LGBMRegressor(objective='multiclass', num_leaves=31, learning_rate=0.05, num_classes=num_classes)
-    # gbm.fit(X_train, y_train)
-    # y_pred = gbm.predict(X_test, num_iteration=gbm.best_iteration_)
-    # multi_eval(y_test, y_pred, result_full_filepath, model_name)
-
-    # learning_rate = 0.001
-    learning_rates = [0.01, 0.001, 0.0005]
-    optimizer_str_list = ["Adam", "Adagrad"]
-    layer_1_num_list = [2, 4, 8, 32, 128, 256, 512, 768, 1024]
-    layer_2_num_list = [2, 4, 8, 32, 128, 256, 512, 768, 1024]
-    for optimizer_str in optimizer_str_list:
-        for layer_1_num in layer_1_num_list:
-            for layer_2_num in layer_2_num_list:
-                for learning_rate in learning_rates:
-
-                    hidden_feature_list = [layer_1_num, layer_2_num]
-                    model_name = f"MFCN_MSE_opt{optimizer_str}_h{layer_1_num}_h{layer_2_num}_lr{learning_rate}"
-                    print(model_name)
-                    y_pred, end_epoch = NN_2layer_train_test(X_train, X_test, y_train, y_test, num_classes, 2000, sklearn_random=sklearn_random,
-                                                             criterion_type="MSE", learning_rate=learning_rate, optimizer_str=optimizer_str,
-                                                             model_str="multi_layers_FCN", hidden_feature_list=hidden_feature_list, model_save_folder_path=model_save_folder_path, model_name_for_save=model_name)
-                    multi_eval(y_test, y_pred, result_full_filepath, model_name, end_epoch)
-
 
     f = open(result_full_filepath, "a+")
     f.write("\n")
