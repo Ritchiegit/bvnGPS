578f61dea0510354a53ce3d498ac4e95d7c70546
Qizhi Li <34611669+Ritchiegit@users.noreply.github.com>
2021-04-20 10:30:56 +0800

    Update predict_and_roc.py

--- predict_and_roc.py
+++ predict_and_roc.py
@@ -1,15 +1,15 @@
 import pandas as pd
 from load_data.load_data_raw import load_data_raw
 from sklearn.model_selection import train_test_split
-from utils import load_list_of_tuple, list_with_index
+from utils import load_list_of_tuple, list_with_index, check_front_of_name, check_end_of_name
 from data_processing.process_data_label import get_label_multilabel
 from data_processing.iPAGE import calculate_delta_and_relative_expression
 import gc
 import numpy as np
-from summary_and_test import summary_and_test
 import os
 import argparse
-from summary_and_test import test_sklearn, test_pytorch, test_lda_RF
+from test_model import test_sklearn, test_pytorch, test_lda_RF
+
 
 def val_with_one_dataset(data, label, model_path, model_type, model_name, result_final_save_file):
     AUC_0, AUC_1, AUC_2 = -1, -1, -1
@@ -21,12 +21,6 @@ def val_with_one_dataset(data, label, model_path, model_type, model_name, result
     elif model_type == "sklearn":
         AUC_0, AUC_1, AUC_2, y_pred, y_pred_3 = test_sklearn(model_path, data, label, result_final_save_file, model_name)
     return AUC_0, AUC_1, AUC_2, y_pred, y_pred_3
-
-# 结果应该存在所属文件夹旁边 的 valdatasetname 文件夹中
-
-
-def check_front_of_name(name, front):
-    return name[:len(front)] == front
 
 
 if __name__ == "__main__":
@@ -61,10 +55,6 @@ if __name__ == "__main__":
 
     # 处理数据
     dataset_random_state = 1
-    # gene_GSE, label_GSE = load_data_raw(dataset=dataset)
-    # label_GSE_concated = pd.concat(label_GSE, axis=0)
-    # gene_GSE_concated = pd.concat(gene_GSE, join="inner", axis=1)
-    # gene_GSE_concated = gene_GSE_concated.T
 
 
     if test_mode == "exclude_exter_val":
@@ -89,8 +79,6 @@ if __name__ == "__main__":
     label = get_label_multilabel(label_GSE_concated=label_GSE_concated)
     data = calculate_delta_and_relative_expression(pair_after_lasso, gene_GSE_concated)
 
-    # 这里和 pickle 比较一下是否完全相同
-    # 没法比较，直接比较 对比 validation的结果吧
     gc.collect()
     data = data.astype(np.float32)  # before astype object; after astype float32
     label = label.astype(np.int64)
@@ -107,7 +95,10 @@ if __name__ == "__main__":
         os.makedirs(result_final_save_path)
     for model_one_folder_name in model_one_folder_name_list:
         # if not check_front_of_name(model_one_folder_name, "2"):
+        print(model_one_folder_name)
         if not check_front_of_name(model_one_folder_name, "2"):
+            continue
+        if not check_end_of_name(model_one_folder_name, "model"):
             continue
         model_folder_name = model_total_folder_name_path + model_one_folder_name + "/"  # results/20210325_external_57065_2_iPAGE_all_exclude_57065/20210325_external_57065_2_iPAGE_all_exclude_57065_seed111_dataRS1_model
         model_list = os.listdir(model_folder_name)  #
@@ -134,15 +125,8 @@ if __name__ == "__main__":
             else:
                 assert check_front_of_name(model_name, "RandomForest"), print(model_name)
                 continue
-                # input()
-            # pred_file = model_pred_path + "/one" + model_name + ".csv"
-            # print(y_pred)
-            # y_pred_pd = pd.DataFrame(y_pred)
-            # y_pred_pd.to_csv(pred_file, index=False, header=False)
 
             pred_file = model_pred_path + "/three" + model_name + ".csv"
             # print(y_pred_3)
             y_pred_3_pd = pd.DataFrame(y_pred_3)
             y_pred_3_pd.to_csv(pred_file, index=False, header=False)
-        # model_path = "final_model_results/20210323_1_data37_SVM/20210323_1_data37_SVM_iPAGE_coco_nc2020_seed102_dataRS1_model/"
-        # summary_and_test(data, label, model_path, result_final_save_file, local_time=0, sklearn_random=1)
