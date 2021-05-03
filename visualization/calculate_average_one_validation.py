083f61df7c9a4241b92b94cf6eaef35b17bad91b
Qizhi Li <34611669+Ritchiegit@users.noreply.github.com>
2021-05-03 18:14:33 +0800

    Update calculate_average_one_validation.py

--- visualization/calculate_average_one_validation.py
+++ visualization/calculate_average_one_validation.py
@@ -1,29 +1,13 @@
 import pandas as pd
 import glob
+source_folder = "../results/final_model_results/20210416_3_smx/predict_onall_exclude2_6269_1_raw639900.7_validation/2*.csv"
+final_average_csv_path = "../results/final_model_results/20210416_3_smx/predict_onall_exclude2_6269_1_raw639900.7_validation/train_0416_3_average.csv"
 
-source_folder = "../results/final_model_results/20210408_1_e2_raw63990_iPAGE/predict_onall_exclude_21802_570650.7_validation/2*.csv"
-final_average_csv_path = "../results/final_model_results/20210408_1_e2_raw63990_iPAGE/predict_onall_exclude_21802_570650.7_validation/0.7average.csv"
-#
-# source_folder = "../results/final_model_results/20210408_1_e2_raw63990_iPAGE/predict_ononly_21802_57065None_validation/2*.csv"
-# final_average_csv_path = "../results/final_model_results/20210408_1_e2_raw63990_iPAGE/predict_ononly_21802_57065None_validation/only_21802_57065.csv"
-source_folder = "../results/final_model_results/20210416_1/2*.csv"
-final_average_csv_path = "../results/final_model_results/20210416_1/test_average.csv"
-# source_folder = "../results/final_model_results/20210416_2/2*.csv"
-# final_average_csv_path = "../results/final_model_results/20210416_2/test_average.csv"
-source_folder = "../results/final_model_results/20210416_1/predict_ononly_21802_57065None_validation/2*.csv"
-final_average_csv_path = "../results/final_model_results/20210416_1/val_0416_1_average.csv"
-source_folder = "../results/final_model_results/20210416_2/predict_ononly_21802_57065None_validation/2*.csv"
-final_average_csv_path = "../results/final_model_results/20210416_2/val_0416_2_average.csv"
+# source_folder = "../results/final_model_results/20210416_3_smx/predict_onall_exclude2_6269_1_raw639900.3_validation/2*.csv"
+# final_average_csv_path = "../results/final_model_results/20210416_3_smx/predict_onall_exclude2_6269_1_raw639900.3_validation/test_0416_3_average.csv"
 
-source_folder = "../results/final_model_results/20210416_2/predict_onall_exclude2_6269_1_raw639900.7_validation/2*.csv"
-final_average_csv_path = "../results/final_model_results/20210416_2/predict_onall_exclude2_6269_1_raw639900.7_validation/train_0416_2_average.csv"
-
-source_folder = "../results/final_model_results/20210416_2/predict_onall_exclude2_6269_1_raw639900.3_validation/2*.csv"
-final_average_csv_path = "../results/final_model_results/20210416_2/predict_onall_exclude2_6269_1_raw639900.3_validation/test_0416_2_average.csv"
-
-
-source_folder = "../results/final_model_results/20210416_2/predict_ononly_21802_57065None_validation/2*.csv"
-final_average_csv_path = "../results/final_model_results/20210416_2/predict_ononly_21802_57065None_validation/val_0416_2_new_average.csv"
+# source_folder = "../results/final_model_results/20210416_3_smx/predict_ononly_21802_57065None_validation/2*.csv"
+# final_average_csv_path = "../results/final_model_results/20210416_3_smx/predict_ononly_21802_57065None_validation/val_0416_3_average.csv"
 
 
 final_result_list = glob.glob(source_folder)
@@ -34,15 +18,7 @@ acc_num = 0
 for final_result_list_each in final_result_list:
     # print(final_result_list_each)
     final_each_pd = pd.read_csv(final_result_list_each, header=None, index_col=0)
-    # if 'lightgbm' in final_each_pd.index:
-    #     final_each_pd.drop(['CART', 'RandomForest', 'lightgbm'], axis=0, inplace=True)
-    # else:
-    #     final_each_pd.drop(['CART', 'RandomForest'], axis=0, inplace=True)
-    # print(final_each_pd)
-    # print(final_each_pd[1])
-    # input()
-    # final_each_pd = final_each_pd.values[:330]  # TODO 1
-    final_each_pd = final_each_pd.values  # TODO 1
+    final_each_pd = final_each_pd.values
 
     if final_each_pd.shape[0] == 120:
         print(final_result_list_each)
@@ -53,8 +29,7 @@ for final_result_list_each in final_result_list:
 # input()
 final_result_aver = final_result_acc / acc_num
 print(final_result_aver)
-final_result_aver = pd.DataFrame(final_result_aver)  # TODO 2
-# final_result_aver.to_csv(final_average_csv_path)
+final_result_aver = pd.DataFrame(final_result_aver)
 
 final_each_pd = pd.read_csv(final_result_list[11], header=None, index_col=0)
 final_result_aver.index = final_each_pd.index
